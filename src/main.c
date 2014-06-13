c6893f39fd5e8c4f8f04385b2499379b63db3013
iamn1ck <iamn1ck@yahoo.com>
2014-06-12 21:02:03 -0500

    3DS 2048 Clone
    Can draw the board and add (psuedo)random pieces currently. Need to fix
    user input.

--- src/main.c
+++ src/main.c
@@ -1,96 +1,318 @@
 ﻿#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
 #include <stdint.h>
 #include <time.h>
 #include <signal.h>
+#include <stdbool.h>
 #include "HID.h"
+#include "memory.h"
+#include "main.h"
+#include "utils.h"
+#include "draw.h"
 
 
 
 #define SIZE 4
 uint32_t score = 0;
 
-void getColor(uint16_t value, char *color, size_t length) {
-	uint8_t original[] = { 8, 255, 1, 255, 2, 255, 3, 255, 4, 255, 5, 255, 6, 255, 7, 255, 9, 0, 10, 0, 11, 0, 12, 0, 13, 0, 14, 0, 255, 0, 255, 0 };
-	//uint8_t blackwhite[] = {232,255,234,255,236,255,238,255,240,255,242,255,244,255,246,0,248,0,249,0,250,0,251,0,252,0,253,0,254,0,255,0};
-	//uint8_t bluered[] = {235,255,63,255,57,255,93,255,129,255,165,255,201,255,200,255,199,255,198,255,197,255,196,255,196,255,196,255,196,255,196,255};
-	uint8_t *scheme = original;
-	uint8_t *background = scheme + 0;
-	uint8_t *foreground = scheme + 1;
-	if (value > 0) while (value >>= 1) {
-		if (background + 2<scheme + sizeof(original)) {
-			background += 2;
-			foreground += 2;
-		}
-	}
-	snprintf(color, length, "\033[38;5;%d;48;5;%dm", *foreground, *background);
-}
-
-struct board
+struct Board
 {
-	int xcoord;
-	int ycoord;
+	int value;
+	int xpos, ypos;
+};
 
-	bool isupdate = 1;
+void init_board(struct Board board1[4][4])
+{
+	int i,j,x=92, y=229;
+	for (i = 0; i < 4; i++){
+		for (j = 0; j < 4; j++){
+			board1[i][j].value = 0;
+			board1[i][j].ypos = y;
+			board1[i][j].xpos = x;
+			x = x + 58;
+		}
+		x = 92;
+		y = y - 58;
+	}
+
 }
 
-void drawBoard(uint16_t board[SIZE][SIZE]) {
+
+void drawBoard(struct Board board1[4][4]) {
 	int8_t x, y;
-	char color[40], reset[] = "\033[m";
-	printf("\033[H");
 
-	printf("2048.c %17d pts\n\n", score);
+	char c, blockscore[8];
 
-	draw_fillrect(400, 20, 600, 220, 0, 255, 0, TOP_SCREEN);
-
-	int x = 400;
-	int y = 220;
-
-	for (i = 0; i < 4; ++i){}
-		for (j = 0; j < 4; ++j){
-			draw_fillrect(x, y, x + 50, y - 50, 255, 255, 0, TOP_SCREEN);
-			x = x + 50;
-		}
-		y = y - 50;
-	}
-
-	for (y = 0; y<SIZE; y++) {
-		for (x = 0; x<SIZE; x++) {
-			getColor(board[x][y], color, 40);
-			printf("%s", color);
-			printf("       ");
-			printf("%s", reset);
-		}
-		printf("\n");
-		for (x = 0; x<SIZE; x++) {
-			getColor(board[x][y], color, 40);
-			printf("%s", color);
-			if (board[x][y] != 0) {
-				char s[8];
-				snprintf(s, 8, "%u", board[x][y]);
-				int8_t t = 7 - strlen(s);
-				printf("%*s%s%*s", t - t / 2, "", s, t / 2, "");
+	int i, j;
+	for (i = 0; i < 4; i++){
+		for (j = 0; j < 4; j++){
+			draw_fillrect(board1[i][j].xpos, board1[i][j].ypos, board1[i][j].xpos + 50, board1[i][j].ypos - 50, 255, 255, 0, TOP_SCREEN);
+			if (board1[i][j].value != 0)
+			{
+				itoa(board1[i][j].value, blockscore);
+				draw_string(blockscore, board1[i][j].xpos + 23, board1[i][j].ypos - 26, 0, 0, 255, TOP_SCREEN);
 			}
-			else {
-				printf("   ·   ");
-			}
-			printf("%s", reset);
 		}
-		printf("\n");
-		for (x = 0; x<SIZE; x++) {
-			getColor(board[x][y], color, 40);
-			printf("%s", color);
-			printf("       ");
-			printf("%s", reset);
-		}
-		printf("\n");
+
 	}
-	printf("\n");
-	printf("        ←,↑,→,↓ or q        \n");
-	printf("\033[A");
 }
 
+
+int findTarget(struct Board board[SIZE], int8_t x, int8_t stop) {
+	int t;
+	// if the position is already on the first, don't evaluate
+	if (x == 0) {
+		return x;
+	}
+	for (t = x - 1; t >= 0; t--) {
+		if (board[t].value != 0) {
+			if (board[t].value != board[x].value) {
+				// merge is not possible, take next position
+				return t + 1;
+			}
+			return t;
+		}
+		else {
+			// we should not slide further, return this one
+			if (t == stop) {
+				return t;
+			}
+		}
+	}
+	// we did not find a
+	return x;
+}
+
+bool slideArray(struct Board board[4]) {
+	bool success = false;
+	int x, t, stop = 0;
+
+	for (x = 0; x<4; x++) {
+		if (board[x].value != 0) {
+			t = findTarget(board, x, stop);
+			// if target is not original position, then move or merge
+			if (t != x) {
+				// if target is not zero, set stop to avoid double merge
+				if (board[t].value != 0) {
+					score += board[t].value + board[x].value;
+					stop = t + 1;
+				}
+				board[t].value += board[x].value;
+				board[x].value = 0;
+				success = true;
+			}
+		}
+	}
+	return success;
+}
+
+void rotateBoard(struct Board board1[4][4]) {
+	int8_t i, j, n = 4;
+	int tmp;
+	for (i = 0; i<n / 2; i++){
+		for (j = i; j<n - i - 1; j++){
+			tmp = board1[i][j].value;
+			board1[i][j].value = board1[j][n - i - 1].value;
+			board1[j][n - i - 1].value = board1[n - i - 1][n - j - 1].value;
+			board1[n - i - 1][n - j - 1].value = board1[n - j - 1][i].value;
+			board1[n - j - 1][i].value = tmp;
+		}
+	}
+}
+
+//we rotate the board instead of determining the logic to move left right or down because this is easier
+
+bool moveUp(struct Board board[4][4]) {
+	bool success = false;
+	int8_t x;
+	for (x = 0; x<4; x++) {
+		success |= slideArray(board[x]);
+	}
+	return success;
+}
+
+bool moveLeft(struct Board board[4][4]) {
+	bool success;
+	rotateBoard(board);
+	success = moveUp(board);
+	rotateBoard(board);
+	rotateBoard(board);
+	rotateBoard(board);
+	return success;
+}
+
+bool moveDown(struct Board board[4][4]) {
+	bool success;
+	rotateBoard(board);
+	rotateBoard(board);
+	success = moveUp(board);
+	rotateBoard(board);
+	rotateBoard(board);
+	return success;
+}
+
+bool moveRight(struct Board board[4][4]) {
+	bool success;
+	rotateBoard(board);
+	rotateBoard(board);
+	rotateBoard(board);
+	success = moveUp(board);
+	rotateBoard(board);
+	return success;
+}
+
+
+bool findPairDown(struct Board board1[4][4]) {
+	bool success = false;
+	int8_t x, y;
+	for (x = 0; x<4; x++) {
+		for (y = 0; y<4 - 1; y++) {
+			if (board1[x][y].value == board1[x][y + 1].value) return true;
+		}
+	}
+	return success;
+}
+
+int countEmpty(struct Board board[4][4]) {
+	int8_t x, y;
+	int16_t count = 0;
+	for (x = 0; x<4; x++) {
+		for (y = 0; y<4; y++) {
+			if (board[x][y].value == 0) {
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
+bool gameEnded(struct Board board[4][4]) {
+	bool ended = true;
+	if (countEmpty(board)>0) return false;
+	if (findPairDown(board)) return false;
+	rotateBoard(board);
+	if (findPairDown(board)) ended = false;
+	rotateBoard(board);
+	rotateBoard(board);
+	rotateBoard(board);
+	return ended;
+}
+
+void addRandom(struct Board board1[4][4]) {
+	static bool initialized = false;
+	int x, y;
+	int r, len = 0;
+	int n, eligpiece[16][2]; //can put new piece here
+
+
+
+	for (x = 0; x<4; x++) {
+		for (y = 0; y<4; y++) {
+			if (board1[x][y].value == 0) {
+				eligpiece[len][0] = x;
+				eligpiece[len][1] = y;
+				len++;
+			}
+		}
+	}
+
+	if (len>0) {
+		r = TIMER % len;
+		x = eligpiece[r][0];
+		y = eligpiece[r][1];
+		n = ((TIMER % 10) / 9 + 1) * 2;
+		board1[x][y].value = n;
+	}
+}
+
+char key_press(){
+	char button_press;
+	button_press = read_word(HID);
+	if ((!(button_press & BUTTON_LEFT) | !(button_press & BUTTON_Y))){
+		return 'l';
+	}
+	if ((!(button_press & BUTTON_RIGHT) | !(button_press & BUTTON_A))){
+		return 'r';
+	}
+	if ((!(button_press & BUTTON_UP) | !(button_press & BUTTON_X))){
+		return 'u';
+	}
+	if ((!(button_press & BUTTON_DOWN) | !(button_press & BUTTON_B))){
+		return 'd';
+	}
+	if (!(button_press & BUTTON_START)){
+		return 'q';
+	}
+
+
+
+}
+
+
+int main(){
+
+	clearscreen(TOP_SCREEN | BOTTOM_SCREEN);
+	initscreens();
+	struct Board board1[4][4];
+	init_board(board1);
+	addRandom(board1);
+	addRandom(board1);
+	drawBoard(board1);
+	char c, buffer[32];
+	bool success;
+	while (true) {
+		c = key_press();
+		switch (c) {
+		case 'l':	// left arrow
+			success = moveLeft(board1);  break;
+		case 'r':	// right arrow
+			success = moveRight(board1); break;
+		case 'u':	// up arrow
+			success = moveUp(board1);    break;
+		case 'd':	// down arrow
+			success = moveDown(board1);  break;
+		default: success = false;
+		}
+
+		if (success) {
+			drawBoard(board1);
+			addRandom(board1);
+			drawBoard(board1);
+			if (gameEnded(board1)) {
+				initscreens();
+				draw_string("new game?", 50, 50, 0, 0, 255, BOTTOM_SCREEN);
+
+				break;
+			}
+		}
+		if (c == 'q') {
+			draw_string("new game?", 50, 50, 0, 0, 255, BOTTOM_SCREEN);
+			while (true) {
+				c = key_press();
+				if (c == 'y'){
+					initscreens(board1);
+					addRandom(board1);
+					addRandom(board1);
+					drawBoard(board1);
+					break;
+				}
+				else {
+					drawBoard(board1);
+					break;
+				}
+			}
+		}
+	}
+	
+		return 0;
+}
+
+void initscreens(){
+	draw_fillrect(0, 0, 320, 240, 90, 100, 225, BOTTOM_SCREEN);
+	draw_fillrect(0, 0, 400, 240, 90, 100, 225, TOP_SCREEN);
+	draw_fillrect(84, 1, 324, 235, 0, 255, 0, TOP_SCREEN);
+}
+
+/*
 int8_t findTarget(uint16_t array[SIZE], int8_t x, int8_t stop) {
 	int8_t t;
 	// if the position is already on the first, don't evaluate
@@ -366,4 +588,4 @@ int main()
 	}
 
 	return 0;
-}
+	}*/
